0e266f265fbc8a508bb5311039913034cda0a81c
ReverseTM <90096550+ReverseTM@users.noreply.github.com>
2022-10-01 01:26:32 +0300

    Add files via upload

---
+++ Laba2/task1/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* matrix_filling(int* matrix, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        matrix[i] = rand() % 10000;
+    }
+    return matrix;
+}
+
+void output_matrix(int* matrix, int size)
+{   
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", matrix[i]);
+    }
+    printf("\n");
+}
+
+int main(int argc, char* argv[])
+{
+    int size_A = 0, size_B = 0;
+
+    printf("Enter size matrix A: \n");
+    scanf("%d", &size_A);
+    printf("Enter size matrix B: \n");
+    scanf("%d", &size_B);
+
+    if (size_A <= 0 || size_B < 0)
+    {
+        printf("Incorrect input!\n");
+        return 0;
+    }
+
+    int A[size_A], B[size_B], C[size_A];
+
+    matrix_filling(A, size_A);
+    matrix_filling(B, size_B);
+
+    if (size_B == 0)
+    {
+        output_matrix(A, size_A);
+    }
+    else
+    {
+        for (int i = 0; i < size_A; i++)
+        {
+            if (i >= size_B)
+            {
+                C[i] = A[i] + B[size_B - 1];
+            }
+            else
+            {
+                C[i] = A[i] + B[i];
+            }
+        }
+        output_matrix(A, size_A);
+        output_matrix(B, size_B);
+        output_matrix(C, size_A);
+    }
+    return 0;
+}
