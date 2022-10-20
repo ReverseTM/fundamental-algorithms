c8ff857fa18bf52588d9fd73c19f380936ca612a
ReverseTM <SteamGamesMy@mail.ru>
2022-10-20 23:41:35 +0300

    update

--- Laba2/task3/main.c
+++ Laba2/task3/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define PI 3.14
 #define epsilon 0.000000000000000000001
+
+int check_epsilon_validity(double eps)
+{
+    if (!(*(((int*)&eps) + 1) >> (sizeof(int) * 8 - 1)))
+        if (!(*((int*)&eps) == 0 && *((int*)&eps) == 0))
+            return 1;
+    return 0;
+}
 
 int isprime(int x)
 {
@@ -23,7 +33,13 @@ int factorial(int n)
 
 double C(double m, double k)
 {
-    return factorial(m) / (factorial(k) * factorial(m - k));
+    double numerator = 1.0;
+    double denominator = factorial(m - k);
+
+    for (int i = k + 1; i <= m; i++)
+        numerator *= i;
+    
+    return (numerator / denominator);
 }
 
 double _mult(int t) {
@@ -37,6 +53,8 @@ double _mult(int t) {
 
 double limit_for_gamma(double eps)
 {
+    if (!check_epsilon_validity(eps))
+        return -1;
     int t = 2;
     double result = 0.0;
     double result_new = 0.0;
@@ -75,6 +93,8 @@ double function_gamma(double x)
 
 double dichotomy(double (*f)(double x), double a, double b, double eps)
 {
+    if (!check_epsilon_validity(eps))
+        return -1;
     double x = 0.0;
     while (fabs(b - a) > eps) {
         x = (a + b) / 2.0;
@@ -88,6 +108,8 @@ double dichotomy(double (*f)(double x), double a, double b, double eps)
 
 double exhibitor_limit(double eps)
 {
+    if (!check_epsilon_validity(eps))
+        return -1;
     int n = 1;
     double result = 0.0;
     double result_new = 0.0;
@@ -101,11 +123,18 @@ double exhibitor_limit(double eps)
 
 double exhibitor_row(double eps)
 {
+    if (!check_epsilon_validity(eps))
+        return -1;
     int n = 0;
     double element = 0.0;
     double sum = 0.0;
+    double fuc = 1.0;
     do {
-        element = 1.0 / factorial(n);
+        if (n == 0)
+            fuc = 1;
+        else
+            fuc *= n;
+        element = 1.0 / fuc;
         sum += element;
         n++;
     } while (fabs(element) > eps);
@@ -114,6 +143,8 @@ double exhibitor_row(double eps)
 
 double pi_limit(double eps)
 {
+    if (!check_epsilon_validity(eps))
+        return -1;
     int n = 1;
     double result = 0.0;
     double result_new = 4.0;
@@ -127,6 +158,8 @@ double pi_limit(double eps)
 
 double pi_row(double eps)
 {
+    if (!check_epsilon_validity(eps))
+        return -1;
     int n = 1;
     double element = 0.0;
     double sum = 0.0;
@@ -140,6 +173,8 @@ double pi_row(double eps)
 
 double ln2_limit(double eps)
 {
+    if (!check_epsilon_validity(eps))
+        return -1;
     int n = 1;
     double result = 0.0;
     double result_new = 0.0;
@@ -153,6 +188,8 @@ double ln2_limit(double eps)
 
 double ln2_row(double eps)
 {
+    if (!check_epsilon_validity(eps))
+        return -1;
     int n = 1;
     double element = 0.0;
     double sum = 0.0;
@@ -166,6 +203,8 @@ double ln2_row(double eps)
 
 double sqrt2_limit(double eps)
 {
+    if (!check_epsilon_validity(eps))
+        return -1;
     int n = 1;
     double result = 0.0;
     double result_new = -0.5;
@@ -179,13 +218,17 @@ double sqrt2_limit(double eps)
 
 double sqrt2_row(double eps)
 {
+    if (!check_epsilon_validity(eps))
+        return -1;
     int n = 2;
     double element = 0.0;
     double mult = 0.0;
     double mult_new = 1.0;
+    double power = 0.25;
     do {
         mult = mult_new;
-        element = pow(2, pow(2, -n));
+        element = pow(2, power);
+        power *= 0.5;
         mult_new *= element;
         n++;
     } while (fabs(mult_new - mult) > eps);
@@ -194,17 +237,22 @@ double sqrt2_row(double eps)
 
 double gamma_limit(double eps)
 {
+    if (!check_epsilon_validity(eps))
+        return -1;
     eps = 0.01;
     double m = 2;
     double result = 0.0;
     double result_new = 0.0;
     double element = 0.0;
     double sum = 0.0;
+    int fuc = 0;
     do {
         result = result_new;
         sum = 0.0;
+        fuc = 1;
         for (int k = 1; k <= m; k++) {
-            element = (C(m, k) * (pow(-1.0, k) / k) * log(factorial(k)));
+            fuc *= k;
+            element = (C(m, k) * (pow(-1.0, k) / k) * log(fuc));
             sum += element;
         }
         result_new = sum;
@@ -215,6 +263,8 @@ double gamma_limit(double eps)
 
 double gamma_row(double eps)
 {   
+    if (!check_epsilon_validity(eps))
+        return -1;
     eps = 0.0000000000001;
     int k = 3;
     double sum = 0.0;
@@ -231,32 +281,104 @@ double gamma_row(double eps)
     return (sum_new - pow(PI, 2) / 6.0);
 }
 
+/*int check_epsilon_validity_var_1(double eps)
+{
+    if (!(*((char*)&eps + sizeof(double) - 1) >> (sizeof(char) * 8 - 1)))
+        if (!(*((int*)&eps) == 0 && *((int*)&eps) == 0))
+            return 1;
+    return 0;
+}*/
+
+/*double generate_double_value()
+{
+    /*int i = 0;
+    const int values_count = sizeof(double) / sizeof(int);
+
+    int value[values_count];
+
+    for (; i < values_count; i++)
+    {
+        value[i] = rand();
+        value[i] |= ((rand() % 2) << sizeof(int) * 8 - 1);
+    }
+
+    return *((double*)value);*/
+
+    //return ((double)rand() * (1000 + 1000)) / (double)RAND_MAX - 1000;
+//} 
+
 int main(int argc, char *argv[])
 {
     int accuracy = 0;
+    double result = 0.0;
     printf("Enter up to what sign you need accuracy\n");
     scanf("%d", &accuracy);
     double eps = pow(10, -accuracy);
 
-    printf("e with limit: %.8lf\n", exhibitor_limit(eps));
-    printf("e with row: %.8lf\n", exhibitor_row(eps));
-    printf("e with equation: %.8lf\n", dichotomy(function_e, 2.0, 3.0, eps));
+    if ((result = exhibitor_limit(eps)) == -1)
+        printf("Incorrect epsilon\n");
+    else
+        printf("e with limit: %.8lf\n", result);
+    if ((result = exhibitor_row(eps)) == -1)
+        printf("Incorrect epsilon\n");
+    else    
+        printf("e with row: %.8lf\n", exhibitor_row(eps));
+    if ((result = dichotomy(function_e, 2.0, 3.0, eps)) == -1)
+        printf("Incorrect epsilon\n");
+    else    
+        printf("e with equation: %.8lf\n", dichotomy(function_e, 2.0, 3.0, eps));
     printf("-------------------------\n");
-    printf("pi with limit: %.8lf\n", pi_limit(eps));
-    printf("pi with row: %.8lf\n", pi_row(eps));
-    printf("pi with equation: %.8lf\n", dichotomy(function_pi, 3.0, 4.0, eps));
+    if ((result = pi_limit(eps)) == -1)
+        printf("Incorrect epsilon\n");
+    else
+        printf("pi with limit: %.8lf\n", pi_limit(eps));
+    if ((result = pi_row(eps)) == -1)
+        printf("Incorrect epsilon\n");
+    else
+        printf("pi with row: %.8lf\n", pi_row(eps));
+    if ((result = dichotomy(function_pi, 3.0, 4.0, eps)) == -1)
+        printf("Incorrect epsilon\n");
+    else
+        printf("pi with equation: %.8lf\n", dichotomy(function_pi, 3.0, 4.0, eps));
     printf("-------------------------\n");
-    printf("ln2 with limit: %.8lf\n", ln2_limit(eps));
-    printf("ln2 with row: %.8lf\n", ln2_row(eps));
-    printf("ln2 with equation: %.8lf\n", dichotomy(function_ln2, 0.0, 1.0, eps));
+    if ((result = ln2_limit(eps)) == -1)
+        printf("Incorrect epsilon\n");
+    else
+        printf("ln2 with limit: %.8lf\n", ln2_limit(eps));
+    if ((result = ln2_row(eps)) == -1)
+        printf("Incorrect epsilon\n");
+    else
+        printf("ln2 with row: %.8lf\n", ln2_row(eps));
+    if ((result = dichotomy(function_ln2, 0.0, 1.0, eps)) == -1)
+        printf("Incorrect epsilon\n");
+    else
+        printf("ln2 with equation: %.8lf\n", dichotomy(function_ln2, 0.0, 1.0, eps));
     printf("-------------------------\n");
-    printf("sqrt(2) with limit: %.8lf\n", sqrt2_limit(eps));
-    printf("sqrt(2) with row: %.8lf\n", sqrt2_row(eps));
-    printf("sqrt(2) with equation: %.8lf\n", dichotomy(function_sqrt2, 1.0, 2.0, eps));
+    if ((result = sqrt2_limit(eps)) == -1)
+        printf("Incorrect epsilon\n");
+    else
+        printf("sqrt(2) with limit: %.8lf\n", sqrt2_limit(eps));
+    if ((result = sqrt2_row(eps)) == -1)
+        printf("Incorrect epsilon\n");
+    else
+        printf("sqrt(2) with row: %.8lf\n", sqrt2_row(eps));
+    if ((result = dichotomy(function_sqrt2, 1.0, 2.0, eps)) == -1)
+        printf("Incorrect epsilon\n");
+    else
+        printf("sqrt(2) with equation: %.8lf\n", dichotomy(function_sqrt2, 1.0, 2.0, eps));
     printf("-------------------------\n");
-    printf("gamma with limit: %.8lf\n", gamma_limit(eps));
-    printf("gamma with row: %.8lf\n", gamma_row(eps));
-    printf("gamma with equation: %.8lf\n", dichotomy(function_gamma, 0.0, 1.0, eps));
+    if ((result = gamma_limit(eps)) == -1)
+        printf("Incorrect epsilon\n");
+    else
+        printf("gamma with limit: %.8lf\n", gamma_limit(eps));
+    if ((result = gamma_row(eps)) == -1)
+        printf("Incorrect epsilon\n");
+    else
+        printf("gamma with row: %.8lf\n", gamma_row(eps));
+    if ((result = dichotomy(function_gamma, 0.0, 1.0, eps)) == -1)
+        printf("Incorrect epsilon\n");
+    else
+        printf("gamma with equation: %.8lf\n", dichotomy(function_gamma, 0.0, 1.0, eps));
 
     return 0;
 }
